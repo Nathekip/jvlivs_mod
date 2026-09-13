13dcf847ed5050939103985516323e288d9e69f7
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2026-09-13 14:10:48 +0200

    initial commit

---
+++ src/main/java/net/nathekip/jvlivsmod/Config.java
@@ -0,0 +1,9 @@
+package net.nathekip.jvlivsmod;
+
+/**
+ * Configuration placeholder for the JVLIVS mod.
+ */
+public final class Config {
+    private Config() {
+    }
+}
---
+++ src/main/java/net/nathekip/jvlivsmod/ExampleMod.java
@@ -0,0 +1,12 @@
+package net.nathekip.jvlivsmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    public static final String MOD_ID = "jvlivsmod";
+
+    public ExampleMod() {
+        // Mod initialization goes here.
+    }
+}
